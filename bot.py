7eb7ca0f00589b7f8dbc36866b231734e939ef86
Mutapapa <mutapapayt@gmail.com>
2025-09-30 13:11:18 -0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -20,6 +20,14 @@ import discord
 from discord.ext import tasks
 from discord.ui import View, button
 
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+intents.reactions = True
+
+bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 # ================== CORE CONFIG ==================
 GUILD_ID = 1411205177880608831
 
